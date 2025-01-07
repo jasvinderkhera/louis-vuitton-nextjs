3fd145faf5c34555576d0e924fcf24ff17787b95
jasvinderkhera <ankitkhera15@gmail.com>
2025-01-07 20:34:14 +0530

    re-edition added

--- app/page.js
+++ app/page.js
@@ -2,10 +2,44 @@
 export default function Home() {
   return (
     <div>
-      <div className="container hero-section mt-2 h-screen">
-      <video src="/bg-desktop.mp4" autoPlay loop muted className="video-bg" />
+      <div className="container hero-section mt-2 overflow-hidden">
+      <video src="/bg-desktop.mp4" autoPlay loop muted className="" />
         <div className="hero-overlay"></div>
       </div>
+      <section className="max-w-screen-xl mx-auto py-20">
+        <p className="text-center text-xs">
+          ICONIC RE-EDITION
+        </p>
+        <p className="text-center text-3xl mt-5">
+        Louis Vuitton x Murakami
+        </p>
+        <div className="flex gap-3">
+          <a href="" className="box">
+            <img src="re1.webp" alt="" />
+            <div className="bagTitle text-center pt-3">
+              <p className="text-s font-light">Women's Handbags</p>
+            </div>
+          </a> 
+          <a href="" className="box">
+            <img src="re2.webp" alt="" />
+            <div className="bagTitle text-center pt-3">
+              <p className="text-s font-light">Women's Wallets and Small Leather Goods</p>
+            </div>
+          </a> 
+          <a href="" className="box">
+            <img src="re3.webp" alt="" />
+            <div className="bagTitle text-center pt-3">
+              <p className="text-s font-light">Women's Accessories</p>
+            </div>
+          </a> 
+          <a href="" className="box">
+            <img src="re4.webp" alt="" />
+            <div className="bagTitle text-center pt-3">
+              <p className="text-s font-light">Women's Shoes</p>
+            </div>
+          </a> 
+        </div>
+      </section>
     </div>
   );
 }
